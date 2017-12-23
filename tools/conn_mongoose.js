fa8eeb3450f8d1a4780b07de1ecffd6210b68a1e
DiRTLC <lc1067938541@163.com>
2017-12-23 17:57:17 +0800

    deleteOne

--- tools/conn_mongoose.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose')
-
-mongoose.connect("mongodb://127.0.0.1/time", {useMongoClient: true})
-mongoose.connection.once("open", function () {
-  console.log('数据库连接成功');
-})
